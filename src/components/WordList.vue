660e4e1ec63e0a970362bffb8776da383df60276
Ashsajal <ashsajal1@gmail.com>
2025-03-26 15:35:29 +0600

    feat: refactor WordList component to use WordCard for displaying word details

--- src/components/WordList.vue
+++ src/components/WordList.vue
@@ -39,24 +39,11 @@
       </div>
 
       <!-- Word cards -->
-      <div
+      <WordCard
         v-for="(word, index) in filteredWords"
         :key="index"
-        class="bg-white dark:bg-gray-800 p-4 rounded-lg shadow"
-      >
-        <div class="flex justify-between items-start">
-          <div>
-            <h3 class="font-bold">{{ word.en }} - {{ word.bn }}</h3>
-            <p class="text-sm text-gray-600 dark:text-gray-400 mt-1">
-              Battle: {{ word.battle }}
-            </p>
-          </div>
-        </div>
-        <div class="mt-2">
-          <p class="text-sm">{{ word.sentence }}</p>
-          <p class="text-sm mt-1">{{ word.bnSentence }}</p>
-        </div>
-      </div>
+        :word="word"
+      />
     </div>
   </div>
 </template>
@@ -64,6 +51,7 @@
 <script setup lang="ts">
 import { ref, onMounted, watch, computed } from "vue";
 import Select from "primevue/select";
+import WordCard from "./WordCard.vue";
 
 interface Word {
   id: number;
