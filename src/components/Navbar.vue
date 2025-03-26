fdd5b521edb15d55ffc8144d50b5799df1d19db9
Ashsajal <ashsajal1@gmail.com>
2025-03-26 19:20:36 +0600

    feat: add settings button to Navbar component and adjust icon sizes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,38 +1,49 @@
 <template>
-    <nav class="flex items-center justify-between p-4 border-b dark:border-b-gray-700">
-        <RouterLink to="/" class="text-2xl font-bold select-none cursor-pointer">Logo</RouterLink>
+  <nav
+    class="flex items-center justify-between p-4 border-b dark:border-b-gray-700"
+  >
+    <RouterLink to="/" class="text-2xl font-bold select-none cursor-pointer"
+      >Logo</RouterLink
+    >
 
-        <div class="flex items-center justify-between gap-2">
-            <Button variant="outlined" @click="next()">
-                <SunDim :size="20" v-if="mode === 'dark'" />
-                <Moon :size="20" v-if="mode === 'light'" />
-                <MonitorDot :size="20" v-if="mode === 'auto'" />{{ mode }}
-            </Button>
-            <RouterLink to="/about">
-                <Button>About</Button>
-            </RouterLink>
-            <RouterLink to="/import">
-                <Button>Import json</Button>
-            </RouterLink>
-        </div>
-    </nav>
+    <div class="flex items-center justify-between gap-2">
+      <Button rounded variant="outlined" @click="next()">
+        <SunDim :size="20" v-if="mode === 'dark'" />
+        <Moon :size="20" v-if="mode === 'light'" />
+        <MonitorDot :size="16" v-if="mode === 'auto'" />{{ mode }}
+      </Button>
+      <RouterLink to="/settings">
+        <Button severity="ghost"
+          ><Settings stroke-width="1.5" :size="20"
+        /></Button>
+      </RouterLink>
+      <RouterLink to="/about">
+        <Button>About</Button>
+      </RouterLink>
+      <RouterLink to="/import">
+        <Button>Import json</Button>
+      </RouterLink>
+    </div>
+  </nav>
 </template>
 
 <script setup lang="ts">
-import { useColorMode, useCycleList } from '@vueuse/core'
-import { Button } from 'primevue';
-import { watchEffect } from 'vue'
-import { SunDim, Moon, MonitorDot } from 'lucide-vue-next';
+import { useColorMode, useCycleList } from "@vueuse/core";
+import { Button } from "primevue";
+import { watchEffect } from "vue";
+import { SunDim, Moon, MonitorDot, Settings } from "lucide-vue-next";
 
 const mode = useColorMode({
-    emitAuto: true,
-    modes: {
-        contrast: 'dark contrast',
-        cafe: 'cafe',
-    },
-})
+  emitAuto: true,
+  modes: {
+    contrast: "dark contrast",
+    cafe: "cafe",
+  },
+});
 
-const { state, next } = useCycleList(['dark', 'light', 'auto'] as const, { initialValue: mode })
+const { state, next } = useCycleList(["dark", "light", "auto"] as const, {
+  initialValue: mode,
+});
 
-watchEffect(() => mode.value = state.value)
+watchEffect(() => (mode.value = state.value));
 </script>
