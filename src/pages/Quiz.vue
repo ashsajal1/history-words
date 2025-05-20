0cae82099e1a7ac18e503af6138611240e0b4aaf
Ashsajal <ashsajal1@gmail.com>
2025-05-20 20:48:32 +0600

    fix: typo

---
+++ src/pages/Quiz.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script setup lang="ts">
+import { useWordStore } from "../store/wordStore";
+
+const wordStore = useWordStore();
+const { words } = wordStore;
+console.log(words);
+</script>
